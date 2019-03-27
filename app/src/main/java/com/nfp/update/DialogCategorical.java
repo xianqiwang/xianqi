5bc77daeb2a6e4459c7cfdcd736a074331760a2d
gengyingbo <yingbo.geng@sim.com>
2019-03-27 10:26:07 +0800

    [categorical]

---
+++ app/src/main/java/com/nfp/update/DialogCategorical.java
@@ -0,0 +1,11 @@
+package com.nfp.update;
+
+public class DialogCategorical {
+/*
+* 每个页面的对话框在这里调用
+* dialog的形成在DefDialog.java里面调用
+* 若果进度调不满足需求就用ProgressActivity.java/TestProgress.java
+* 
+* */
+
+}
