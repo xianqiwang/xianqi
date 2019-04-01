fcf1f783f4d90aadec859ea0e5c87d042bfbdf09
gengyingbo <yingbo.geng@sim.com>
2019-04-01 09:27:03 +0800

    wheelpicker

--- app/src/main/java/com/nfp/update/DefDialog.java
+++ app/src/main/java/com/nfp/update/DefDialog.java
@@ -107,7 +107,9 @@ class DefDialog extends Dialog {
 /*
                 android.util.Log.v("yingbo","onOkKey");
 */
-                mOnCenterKeyListener.onOkKey();
+/*                mOnCenterKeyListener.onOkKey();
+                setWheelPickerVisible();*/
+                android.util.Log.v ("yingbo","value"+getWheelPickerCurrentValue ());
 
                 /*
                 setViewOneGone();
@@ -123,7 +125,9 @@ class DefDialog extends Dialog {
             @Override
             public void onClick (android.view.View v) {
               android.util.Log.v("yingbo","onCenterKey");
-
+/*
+              setWheelPickerCurrentValue (3);
+*/
                 i--;
 /*
                 setSpinnerTextSize (i);
@@ -152,6 +156,39 @@ class DefDialog extends Dialog {
         start();
     }
 
+    int mPosition = 0;
+
+    public int getWheelPickerCurrentValue(){
+
+
+        wheelPicker.setOnItemSelectedListener (new WheelPicker.OnItemSelectedListener(){
+
+            /**
+             * 当滚轮选择器数据项被选中时回调该方法
+             * 滚动选择器滚动停止后会回调该方法并将当前选中的数据和数据在数据列表中对应的位置返回
+             *
+             * @param picker   滚轮选择器
+             * @param data     当前选中的数据
+             * @param position 当前选中的数据在数据列表中的位置
+             */
+            @Override
+            public void onItemSelected (com.nfp.update.WheelPicker picker, Object data, int position) {
+
+                mPosition = position;
+
+            }
+        });
+        return mPosition;
+    }
+    public void setWheelPickerVisible(){
+        wheelPicker.setVisibility (android.view.View.VISIBLE);
+    }
+    public void setWheelPickerGone(){
+        wheelPicker.setVisibility (android.view.View.GONE);
+    }
+    public void setWheelPickerCurrentValue(int itemId){
+        wheelPicker.setSelectedItemPosition (itemId);
+    }
 
     public void setWheelPicker () {
         List<String>items =new ArrayList<String> ();
